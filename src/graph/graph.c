89eae02f418390d66b45167ab1cfaad5218e74e5
Thibault Villepreux <thibault.villepreux@gmail.com>
2023-06-27 14:11:30 +0200

    recuperation structure graphe

--- src/graph/graph.c
+++ src/graph/graph.c
@@ -1,6 +1,32 @@
 #include "graph.h"
+#include "stdlib.h"
+
 
 int graph_main(void)
 {
     return 0;
+}
+
+
+/**
+ * \fn void genere(graphe_t ** matrice, int bas, int haut)
+ * \brief Fonction qui génère un graphe connexe aléatoire
+ * 
+ *  
+ * \param[in] 
+ * \param[in] 
+ * \param[in] 
+ * 
+ * \return void : pas de retour, effet de bord sur le graphe
+ * 
+ */
+void genere(graphe_t matrice, unsigned int bas, unsigned int haut)
+{
+    int k = 0; 
+    if (bas < haut)
+    {
+	k = rand() % (haut - (bas +1) +1) + (bas +1); 
+	matrice[bas][bas +1] = 1;
+	
+    }
 }
