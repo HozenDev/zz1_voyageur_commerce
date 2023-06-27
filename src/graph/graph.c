2bf2390fb102da69cc81b328bcfcc1eaa90eefce
Thibault Villepreux <thibault.villepreux@gmail.com>
2023-06-27 14:32:01 +0200

    ajout graphe connexe

--- src/graph/graph.c
+++ src/graph/graph.c
@@ -2,7 +2,7 @@
 #include "stdlib.h"
 
 
-void graph_print_sdl
+
 
 int graph_main(void)
 {
@@ -15,20 +15,32 @@ int graph_main(void)
  * \brief Fonction qui génère un graphe connexe aléatoire
  * 
  *  
- * \param[in] 
- * \param[in] 
- * \param[in] 
+ * \param[in] graph_t graph
+ * \param[in] unsigned short down
+ * \param[in] unsigned short up
  * 
  * \return void : pas de retour, effet de bord sur le graphe
  * 
  */
-void genere(graphe_t matrice, unsigned int bas, unsigned int haut)
+void graph_generate_related(graph_t graph, unsigned short down, unsigned short up)
 {
-    int k = 0; 
-    if (bas < haut)
+    unsigned int k = 0; 
+    if (down < up)
     {
-	k = rand() % (haut - (bas +1) +1) + (bas +1); 
-	matrice[bas][bas +1] = 1;
-	
+	k = rand() % (up - (down +1) +1) + (down +1); 
+	graph.matrix[down][down +1] = 1;
+	graph.matrix[down+1][down] = 1;
+	if (k+1 <= up)
+	{
+	    graph.matrix[down][k+1] = 1;
+	    graph.matrix[k+1][down] = 1;
+	    graph_generate_related(graph, down+1, k);
+	    graph_generate_related(graph, k+1, up);
+	}
     }
 }
+
+
+
+
+void graph_initialize_graph();
--- src/graph/graph.h
+++ src/graph/graph.h
@@ -2,7 +2,7 @@
 #define _graph_h_
 
 struct graph_s {
-    char ** g;
+    char ** matrix;
     unsigned short w;
     unsigned short h;
 };
@@ -11,4 +11,21 @@ typedef struct graph_s graph_t;
 
 int graph_main(void);
 
+/**
+ * \fn void genere(graphe_t ** matrice, int bas, int haut)
+ * \brief Fonction qui génère un graphe connexe aléatoire
+ * 
+ *  
+ * \param[in] graph_t graph
+ * \param[in] unsigned short down
+ * \param[in] unsigned short up
+ * 
+ * \return void : pas de retour, effet de bord sur le graphe
+ * 
+ */
+void graph_generate_related(graph_t graph, unsigned short down, unsigned short up);
+
+void graph_initialize_graph();
+
+    
 #endif
