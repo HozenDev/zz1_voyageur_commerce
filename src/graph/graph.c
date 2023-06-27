09f521bc242b9d7e15226a649b1c0cb6ae81e269
Hozen <enzo.durel@gmail.com>
2023-06-27 14:54:07 +0200

    commencement graph sdl

--- src/graph/graph.c
+++ src/graph/graph.c
@@ -1,6 +1,52 @@
 #include "graph.h"
 
-void graph_print_sdl
+/**
+ * @brief Generate coordiantes of a point
+ *
+ * @param width, screen width point
+ * @param height, screen height point
+ * @param offset_x, offset_x of the point
+ * @param offset_y, offset_y of the point
+ */
+SDL_Point graph_generate_point(int width, int height, int offset_x, int offset_y)
+{
+    SDL_Point p;
+
+    p.x = rand()%(width-offset_x) + offset_x;
+    p.y = rand()%(height-offset_y) + offset_y;
+    
+    return p;
+}
+
+/**
+ * @brief Generate coordiantes points for a graph
+ *
+ * @param graph_sdl_t ** g, graph to generate point
+ * @param width, screen width point
+ * @param height, screen height point
+ * @param ratio, ratio for compute offsets
+ */
+graph_sdl_t * graph_generate_sdl(graph_sdl_t * g, int width, int height, int ratio)
+{
+    int offset_x = width*ratio;
+    int offset_y = height*ratio;
+
+    int i;
+
+    for (i = 0; i < g->g.n; ++i)
+    {
+        g.
+    }
+}
+
+/**
+ *
+ *
+ */
+void graph_print_sdl(SDL_Renderer * renderer, graph_sdl_t * g)
+{
+    
+}
 
 int graph_main(void)
 {
--- src/graph/graph.h
+++ src/graph/graph.h
@@ -1,14 +1,22 @@
 #ifndef _graph_h_
 #define _graph_h_
 
+#include "../sdl/sdl.h"
+
 struct graph_s {
     char ** g;
-    unsigned short w;
-    unsigned short h;
+    unsigned short n;
 };
 
 typedef struct graph_s graph_t;
 
+struct graph_sdl_s {
+    struct graph_s g;
+    SDL_Point * p;
+};
+
+typedef struct graph_sdl_s graph_sdl_t;
+    
 int graph_main(void);
 
 #endif
