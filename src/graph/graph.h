8b3b2a7a83af66963e6c09d8b9c1ad9351c95aba
Thibault Villepreux <thibault.villepreux@gmail.com>
2023-06-27 15:45:06 +0200

    ajout fonctions affichages dans un file

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -22,21 +22,12 @@ typedef struct graph_sdl_s graph_sdl_t;
     
 int graph_main(void);
 
-/**
- * \fn void genere(graphe_t ** matrice, int bas, int haut)
- * \brief Fonction qui génère un graphe connexe aléatoire
- * 
- *  
- * \param[in] graph_t graph
- * \param[in] unsigned short down
- * \param[in] unsigned short up
- * 
- * \return void : pas de retour, effet de bord sur le graphe
- * 
- */
 void graph_generate_related(graph_t * graph, unsigned short down, unsigned short up);
 
 graph_t *  graph_initialize_graph(unsigned short n);
 
-    
+void print_graph_file_pretty(FILE * flux, graph_t * graph);
+
+void print_graph_file(FILE * flux, graph_t * graph);
+
 #endif
